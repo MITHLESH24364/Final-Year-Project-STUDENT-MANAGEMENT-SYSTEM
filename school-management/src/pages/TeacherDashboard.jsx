7641fe8e320161c9730e8f45531def29d1784c06
sandesb <bajracharyasandeshh@gmail.com>
2024-11-17 22:20:55 +0545

    check

--- school-management/src/pages/TeacherDashboard.jsx
+++ school-management/src/pages/TeacherDashboard.jsx
@@ -1,11 +1,183 @@
-import React from 'react'
+import React from "react";
+import SimpleCalendar from "simple-calendar-react-library";
+// import "simple-calendar-react-library/dist/index.css";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+// import "react-circular-progressbar/dist/styles.css";
 
 const TeacherDashboard = () => {
   return (
-    <div>
-      <h1>Teachers</h1>
+    <div className="main-wrapper">
+      <div className="page-wrapper">
+        <div className="content container-fluid">
+          {/* Page Header */}
+          <div className="page-header">
+            <div className="row">
+              <div className="col-sm-12">
+                <div className="page-sub-header">
+                  <h3 className="page-title">Welcome Jonathan!</h3>
+                  <ul className="breadcrumb">
+                    <li className="breadcrumb-item"><a href="/">Home</a></li>
+                    <li className="breadcrumb-item active">Teacher</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Dashboard Stats */}
+          <div className="row">
+            {[
+              { title: "Total Classes", count: "04/06", icon: "teacher-icon-01.svg" },
+              { title: "Total Students", count: "40/60", icon: "dash-icon-01.svg" },
+              { title: "Total Lessons", count: "30/50", icon: "teacher-icon-02.svg" },
+              { title: "Total Hours", count: "15/20", icon: "teacher-icon-03.svg" },
+            ].map((stat, index) => (
+              <div className="col-xl-3 col-sm-6 col-12 d-flex" key={index}>
+                <div className="card bg-comman w-100">
+                  <div className="card-body">
+                    <div className="db-widgets d-flex justify-content-between align-items-center">
+                      <div className="db-info">
+                        <h6>{stat.title}</h6>
+                        <h3>{stat.count}</h3>
+                      </div>
+                      <div className="db-icon">
+                        <img
+                          src={`assets/img/icons/${stat.icon}`}
+                          alt="Dashboard Icon"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* Main Content */}
+          <div className="row">
+            {/* Upcoming Lessons */}
+            <div className="col-12 col-lg-8 col-xl-8 d-flex">
+              <div className="card flex-fill comman-shadow">
+                <div className="card-header">
+                  <div className="row align-items-center">
+                    <div className="col-6">
+                      <h5 className="card-title">Upcoming Lesson</h5>
+                    </div>
+                    <div className="col-6">
+                      <span className="float-end view-link">
+                        <a href="#">View All Courses</a>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="pt-3 pb-3">
+                  <div className="table-responsive lesson">
+                    <table className="table table-center">
+                      <tbody>
+                        {Array.from({ length: 2 }).map((_, idx) => (
+                          <tr key={idx}>
+                            <td>
+                              <div className="date">
+                                <b>Lessons 30</b>
+                                <p>3.1 Ipsum dolor</p>
+                                <ul className="teacher-date-list">
+                                  <li>
+                                    <i className="fas fa-calendar-alt me-2"></i>Sep 5, 2022
+                                  </li>
+                                  <li>|</li>
+                                  <li>
+                                    <i className="fas fa-clock me-2"></i>09:00 - 10:00 am
+                                  </li>
+                                </ul>
+                              </div>
+                            </td>
+                            <td>
+                              <div className="lesson-confirm">
+                                <a href="#">Confirmed</a>
+                              </div>
+                              <button type="submit" className="btn btn-info">
+                                Reschedule
+                              </button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Semester Progress */}
+            <div className="col-12 col-lg-4 col-xl-4 d-flex">
+              <div className="card flex-fill comman-shadow">
+                <div className="card-header">
+                  <h5 className="card-title">Semester Progress</h5>
+                </div>
+                <div className="dash-widget">
+                  <CircularProgressbar
+                    value={92}
+                    text={`55/60`}
+                    styles={buildStyles({
+                      textSize: "16px",
+                      pathColor: "#4CAF50",
+                      textColor: "#4CAF50",
+                      trailColor: "#d6d6d6",
+                    })}
+                  />
+                  <p className="mt-3 text-center">Lesson Progressed</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Teaching Activity */}
+          <div className="row">
+            <div className="col-12 col-lg-12 col-xl-12 d-flex">
+              <div className="card flex-fill comman-shadow">
+                <div className="card-header">
+                  <div className="row align-items-center">
+                    <div className="col-6">
+                      <h5 className="card-title">Teaching Activity</h5>
+                    </div>
+                    <div className="col-6">
+                      <ul className="chart-list-out">
+                        <li><span className="circle-blue"></span>Teacher</li>
+                        <li><span className="circle-green"></span>Students</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div className="card-body">
+                  {/* Placeholder for chart */}
+                  <div id="school-area">Chart Goes Here</div>
+                </div>
+              </div>
+            </div>
+
+            {/* Calendar */}
+            <div className="col-12 col-lg-12 col-xl-4 d-flex">
+              <div className="card flex-fill comman-shadow">
+                <div className="card-body">
+                  <SimpleCalendar
+                    activeMonth={new Date()}
+                    events={[
+                      {
+                        date: new Date(),
+                        title: "Botany Class",
+                        description: "Lorem ipsum sit amet",
+                      },
+                    ]}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default TeacherDashboard;
