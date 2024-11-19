9139f5bff99b7fef96d683083e706f5d476f2017
MITHLESH24364 <singhmithlesh095@gmail.com>
2024-11-19 15:32:59 +0545

    Update add student

--- school-management/src/pages/AddStudent.jsx
+++ school-management/src/pages/AddStudent.jsx
@@ -63,75 +63,245 @@ const AddStudent = () => {
                         </h5>
                       </div>
 
-                      {/* First Name */}
-                      <div className="col-12 col-sm-4">
-                        <div className="form-group">
-                          <label>
-                            First Name <span className="text-danger">*</span>
-                          </label>
-                          <input
-                            type="text"
-                            name="firstName"
-                            className="form-control"
-                            placeholder="Enter First Name"
-                            value={formData.firstName}
-                            onChange={handleChange}
-                          />
-                        </div>
-                      </div>
+                     
+                      
 
-                      {/* Last Name */}
-                      <div className="col-12 col-sm-4">
-                        <div className="form-group">
-                          <label>
-                            Last Name <span className="text-danger">*</span>
-                          </label>
-                          <input
-                            type="text"
-                            name="lastName"
-                            className="form-control"
-                            placeholder="Enter Last Name"
-                            value={formData.lastName}
-                            onChange={handleChange}
-                          />
-                        </div>
-                      </div>
 
-                      {/* Gender */}
-                      <div className="col-12 col-sm-4">
-                        <div className="form-group">
-                          <label>
-                            Gender <span className="text-danger">*</span>
-                          </label>
-                          <select
-                            name="gender"
-                            className="form-control"
-                            value={formData.gender}
-                            onChange={handleChange}
-                          >
-                            <option value="">Select Gender</option>
-                            <option value="Male">Male</option>
-                            <option value="Female">Female</option>
-                            <option value="Others">Others</option>
-                          </select>
-                        </div>
-                      </div>
+                      
 
-                      {/* Date of Birth */}
-                      <div className="col-12 col-sm-4">
-                        <div className="form-group">
-                          <label>
-                            Date Of Birth <span className="text-danger">*</span>
-                          </label>
-                          <DatePicker
-                            className="form-control"
-                            selected={formData.dob}
-                            onChange={handleDateChange}
-                            dateFormat="dd-MM-yyyy"
-                          />
-                        </div>
-                      </div>
 
+
+                   
+        <div className="col-12 col-sm-4">
+          <div className="form-group local-forms">
+            <label>Full Name <span className="login-danger">*</span></label>
+            <input
+              className="form-control"
+              type="text"
+              name="fullname"
+              placeholder="Enter Full Name"
+              value={formData.fullname}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        {/* Gender */}
+        <div className="col-12 col-sm-4">
+          <div className="form-group local-forms">
+            <label>Gender <span className="login-danger">*</span></label>
+            <select
+              className="form-control select"
+              name="gender"
+              value={formData.gender}
+              onChange={handleChange}
+            >
+              <option>Select Gender</option>
+              <option>Female</option>
+              <option>Male</option>
+              <option>Others</option>
+            </select>
+          </div>
+        </div>
+
+        {/* Date of Birth */}
+        <div className="col-12 col-sm-4">
+          <div className="form-group local-forms calendar-icon">
+            <label>Date Of Birth <span className="login-danger">*</span></label>
+            <input
+              className="form-control datetimepicker"
+              type="text"
+              name="dateOfBirth"
+              placeholder="DD-MM-YYYY"
+              value={formData.dateOfBirth}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        {/* Roll No */}
+        <div className="col-12 col-sm-4">
+          <div className="form-group local-forms">
+            <label>Roll No</label>
+            <input
+              className="form-control"
+              type="number"
+              name="rollNo"
+              placeholder="Enter Roll Number"
+              value={formData.rollNo}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        {/* Location */}
+        <div className="col-12 col-sm-4">
+          <div className="form-group local-forms">
+            <label>Location</label>
+            <input
+              className="form-control"
+              type="text"
+              name="location"
+              placeholder="Enter Location"
+              value={formData.location}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        {/* Blood Group */}
+        <div className="col-12 col-sm-4">
+          <div className="form-group local-forms">
+            <label>Blood Group <span className="login-danger">*</span></label>
+            <select
+              className="form-control select"
+              name="bloodGroup"
+              value={formData.bloodGroup}
+              onChange={handleChange}
+            >
+              <option>Please Select Group</option>
+              <option>A+</option>
+              <option>B+</option>
+              <option>O+</option>
+              <option>A-</option>
+              <option>B-</option>
+              <option>O-</option>
+              <option>AB+</option>
+              <option>AB-</option>
+            </select>
+          </div>
+        </div>
+
+        {/* Religion */}
+        <div className="col-12 col-sm-4">
+          <div className="form-group local-forms">
+            <label>Religion <span className="login-danger">*</span></label>
+            <select
+              className="form-control select"
+              name="religion"
+              value={formData.religion}
+              onChange={handleChange}
+            >
+              <option>Please Select Religion</option>
+              <option>Hindu</option>
+              <option>Christian</option>
+              <option>Others</option>
+            </select>
+          </div>
+        </div>
+
+        {/* Email */}
+        <div className="col-12 col-sm-4">
+          <div className="form-group local-forms">
+            <label>E-Mail <span className="login-danger">*</span></label>
+            <input
+              className="form-control"
+              type="text"
+              name="email"
+              placeholder="Enter Email Address"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        {/* Class */}
+        <div className="col-12 col-sm-4">
+          <div className="form-group local-forms">
+            <label>Class <span className="login-danger">*</span></label>
+            <select
+              className="form-control select"
+              name="studentClass"
+              value={formData.studentClass}
+              onChange={handleChange}
+            >
+              <option>Please Select Class</option>
+              <option>12</option>
+              <option>11</option>
+              <option>10</option>
+              <option>9</option>
+              <option>8</option>
+              <option>7</option>
+              <option>6</option>
+              <option>5</option>
+              <option>4</option>
+              <option>3</option>
+              <option>2</option>
+              <option>1</option>
+              <option>Pre II</option>
+              <option>Pre I</option>
+              <option>Foundation</option>
+            </select>
+          </div>
+        </div>
+
+        {/* Section */}
+        <div className="col-12 col-sm-4">
+          <div className="form-group local-forms">
+            <label>Section <span className="login-danger">*</span></label>
+            <select
+              className="form-control select"
+              name="section"
+              value={formData.section}
+              onChange={handleChange}
+            >
+              <option>Please Select Section</option>
+              <option>A</option>
+              <option>B</option>
+              <option>C</option>
+              <option>D</option>
+              <option>E</option>
+            </select>
+          </div>
+        </div>
+
+        {/* Phone Number */}
+        <div className="col-12 col-sm-4">
+          <div className="form-group local-forms">
+            <label>Phone Number</label>
+            <input
+              className="form-control"
+              type="text"
+              name="phoneNumber"
+              placeholder="Enter Phone Number"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        {/* Parent Name */}
+        <div className="col-12 col-sm-4">
+          <div className="form-group local-forms">
+            <label>Parent Name</label>
+            <input
+              className="form-control"
+              type="text"
+              name="parentName"
+              placeholder="Enter Parent Name"
+              value={formData.parentName}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        {/* Parent Number */}
+        <div className="col-12 col-sm-4">
+          <div className="form-group local-forms">
+            <label>Parent Number</label>
+            <input
+              className="form-control"
+              type="text"
+              name="parentNo"
+              placeholder="Enter Parent Phone Number"
+              value={formData.parentNo}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+   
+        
                       {/* Upload Photo */}
                       <div className="col-12 col-sm-4">
                         <div className="form-group">
@@ -145,12 +315,12 @@ const AddStudent = () => {
                         </div>
                       </div>
 
-                      {/* Submit Button */}
-                      <div className="col-12">
-                        <button type="submit" className="btn btn-primary">
-                          Submit
-                        </button>
-                      </div>
+        {/* Submit Button */}
+        <div className="col-12">
+          <div className="student-submit">
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </div>
+        </div>
                     </div>
                   </form>
                 </div>
