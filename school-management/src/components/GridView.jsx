e6fd787b060603a5026d7fad42ddd36685dedcb1
MITHLESH24364 <singhmithlesh095@gmail.com>
2025-01-08 07:41:23 +0545

    Update GridView

--- school-management/src/components/GridView.jsx
+++ school-management/src/components/GridView.jsx
@@ -7,7 +7,7 @@ const GridView = ({ teachers }) => {
         <div className="col-lg-4 col-md-6" key={teacher.id} style={{ marginBottom: "20px" }}>
           <div className="card">
             <img
-              src={teacher.profileImage}
+              src={`assets/img/profiles/${teacher.image || "mks.jpg"}`}
               alt={teacher.name}
               className="card-img-top rounded-circle"
               style={{
@@ -18,10 +18,11 @@ const GridView = ({ teachers }) => {
               }}
             />
             <div className="card-body text-center">
-              <h5 className="card-title">{teacher.name}</h5>
-              <p className="card-text">{teacher.education}</p>
+              <h5 className="card-title">{teacher.fullname}</h5>
+              <p className="card-text">{teacher.department}</p>
               <p><strong>{teacher.gender}</strong></p>
               <p>{teacher.email}</p>
+              <p>{teacher.phoneNumber}</p>
               <button className="btn btn-outline-primary btn-sm">Read More</button>
             </div>
           </div>
